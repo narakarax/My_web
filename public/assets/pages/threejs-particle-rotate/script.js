1e1907c60ec3278c86dbf7480b863163b2066f56
liliyagerenko <liliyaogerenko@gmail.com>
2022-04-10 15:49:56 -0700

    claud + terrain + background

--- public/assets/pages/threejs-particle-rotate/script.js
+++ public/assets/pages/threejs-particle-rotate/script.js
@@ -1,7 +1,7 @@
     $(function () {
         var camera, scene, renderer;
-        var mesh;
-        
+        var mesh, CludMesh;
+        var backgroundMesh;
         init();
         animate();
         
@@ -14,15 +14,36 @@
         
         scene = new THREE.Scene();
         
-        var geometry = new THREE.SphereGeometry(10, 100, 100);
-        var material  = new THREE.MeshPhongMaterial();
+        // var geometry = new THREE.SphereGeometry(10, 100, 100);
+        // var material  = new THREE.MeshPhongMaterial();
         
         THREE.ImageUtils.crossOrigin = '';
-        material.map    = THREE.ImageUtils.loadTexture('http://s3-us-west-2.amazonaws.com/s.cdpn.io/1206469/earthmap1k.jpg')
+        // material.map    = THREE.ImageUtils.loadTexture('http://s3-us-west-2.amazonaws.com/s.cdpn.io/1206469/earthmap1k.jpg')
         
-          mesh = new THREE.Mesh(geometry, material);
-          mesh.rotation.x += 0.5;
+          mesh = new THREE.Mesh(
+            new THREE.SphereGeometry(10, 32, 32),
+            new THREE.MeshPhongMaterial({
+              map: THREE.ImageUtils.loadTexture('./2_no_clouds_4k.jpg'),})
+          );
+          CludMesh = new THREE.Mesh(
+            new THREE.SphereGeometry(10.06, 32, 32),
+            new THREE.MeshPhongMaterial({
+              map: THREE.ImageUtils.loadTexture('./fair_clouds_4k.png'),
+              transparent: true
+            })
+          )
+          backgroundMesh = new THREE.Mesh(
+            new THREE.SphereGeometry(30, 32, 32), 
+            new THREE.MeshBasicMaterial({
+              map: THREE.ImageUtils.loadTexture('./galaxy_starfield.png'), 
+              side: THREE.BackSide
+            })
+          )
+
+          mesh.rotation.x += 0.2;
           scene.add(mesh);
+          scene.add(CludMesh);
+          scene.add(backgroundMesh);
         
           var light1 = new THREE.AmbientLight( 0xffffff );
           light1.position.set(100, 50, 100);
@@ -40,6 +61,7 @@
         function animate() {
           resize();
           mesh.rotation.y += 0.005;
+          CludMesh.rotation.y -= 0.001;
           renderer.render(scene, camera);
           requestAnimationFrame(animate);
         }
